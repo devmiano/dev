c163ad0666b8c3ace5571aa9e0fedfeb019bc0ea
devmiano <dev.miano@gmail.com>
2022-02-26 11:34:27 +0300

    removed bootstrap from the project and from all pages that used it, and from the node modules

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// export default {
-//   css: {
-//     preprocessorOptions: {
-//       scss: {
-//         additionalData: `@import "./styles/global.scss";`,
-//       },
-//     },
-//   },
-// };
